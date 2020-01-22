1c94cf9a1791b3c0019a4bc8c32c75c3d1b8d2cf
Morten Enemark Lund <melund@gmail.com>
2020-01-22 15:01:21 +0100

    fix prefix and c++ 14

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,8 +8,9 @@ cd build
 # Configure using the CMakeFiles
 cmake -G "Ninja"  \
       -DBTK_WRAP_PYTHON:BOOL=1 \
-      -DCMAKE_INSTALL_PREFIX:FILEPATH="${LIBRARY_PREFIX}" \
-      -DCMAKE_PREFIX_PATH:PATH="${LIBRARY_PREFIX}" \
+      -DCMAKE_CXX_STANDARD=14 \
+      -DCMAKE_INSTALL_PREFIX:FILEPATH="${PREFIX}" \
+      -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
       -DPYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT}" \
       -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python${PY_VER}m" \
       -DNUMPY_INCLUDE_DIR="${SP_DIR}/numpy/core/include" \
@@ -21,14 +22,14 @@ cmake -G "Ninja"  \
       ${SRC_DIR}
 
 
-nmake
+ninja
 
-nmake install
+ninja install
 
 # Not sure if we should do this on mac/linux
 # RMDIR /Q/S "%LIBRARY_PREFIX%\share\btk-0.4dev"
 
 # Collect bin/_btk.pyd and bin/btk.py move it to site_packages
 cp "bin/btk.py" "${SP_DIR}/btk.py"
-cpp "bin/_btk${SHLIB_EXT}" "${SP_DIR}/_btk${SHLIB_EXT}"
+cp "bin/_btk${SHLIB_EXT}" "${SP_DIR}/_btk${SHLIB_EXT}"
 
